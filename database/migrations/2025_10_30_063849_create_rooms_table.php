6f37e15f6844ba23b1b7bccdb4a556ba9f4f2784
Nivea Nyctra <rizki.adsu@gmail.com>
2025-10-30 14:39:15 +0700

    base

---
+++ database/migrations/2025_10_30_063849_create_rooms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->integer('price')->default(0);
+            $table->string('size');
+            $table->enum('status', ['available', 'occupied', 'maintenance'])->default('available');
+            $table->string('description');
+            $table->string('image_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
